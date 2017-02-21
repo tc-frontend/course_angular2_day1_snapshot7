2b3c6cbbd600921ca511044cb78dea6de43a2738
tc-frontend <roberto.loreto@avanade.com>
2017-02-21 12:23:22 +0100

    +Add <base href='/'> tag to h +Register routes in app.module +Add welcome and detail component to module declarations +Tyling routes to actions +Add router-outlet directive to show de components -Remove selector in product-list component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms'
 import { HttpModule}  from '@angular/http'
+import { RouterModule } from '@angular/router'
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component'
+import { WelcomeComponent } from './home/welcome.component'
+import { ProductDetailComponent } from './products/product-detail.component'
 import { ProductFilterPipe } from './products/product-filter.pipe'
 import { StarComponent } from './shared/star.component'
 
@@ -12,10 +15,19 @@ import { StarComponent } from './shared/star.component'
   imports: [ 
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'products', component: ProductListComponent},
+      {path:'product/:id', component: ProductDetailComponent},
+      {path:'welcome', component: WelcomeComponent},
+      {path:'', redirectTo: 'welcome', pathMatch: 'full'},
+      {path:'**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   declarations: [ 
     AppComponent,
+    WelcomeComponent,
+    ProductDetailComponent,
     ProductListComponent,
     ProductFilterPipe,
     StarComponent
